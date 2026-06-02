5416a54fa485ae421ea7f39f1fd9a8e014788440
Justyce <justhcoding@gmail.com>
2026-06-02 12:23:46 -0700

    Updated Schema.sql to now utilize trigger functions for building a search vector that uses tsvector and implemented fuzzy search through trgm, added multiple new trigger statements and functions

--- database/schema.sql
+++ database/schema.sql
@@ -1,4 +1,6 @@
-CREATE TABLE users(
+CREATE EXTENSION IF NOT EXISTS pg_trgm;
+
+CREATE TABLE IF NOT EXISTS users(
     user_id SERIAL PRIMARY KEY,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
@@ -10,14 +12,14 @@ CREATE TABLE users(
     account_type VARCHAR(20) DEFAULT 'user' NOT NULL
 );
 
-CREATE TABLE categories(
+CREATE TABLE IF NOT EXISTS categories(
     id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL UNIQUE,
     slug VARCHAR(100) NOT NULL UNIQUE
 );
 
 
-CREATE TABLE businesses(
+CREATE TABLE IF NOT EXISTS businesses(
     id SERIAL PRIMARY KEY,
     owner_user_id INTEGER REFERENCES users(user_id),
     name VARCHAR(100) NOT NULL,
@@ -28,12 +30,12 @@ CREATE TABLE businesses(
     zip VARCHAR(10),
     phone VARCHAR(20) UNIQUE,
     email VARCHAR(255) UNIQUE NOT NULL,
-    slug, VARCHAR(100) UNIQUE NOT NULL,
+    slug VARCHAR(100) UNIQUE NOT NULL,
     created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
-    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
 );
 
-CREATE TABLE services(
+CREATE TABLE IF NOT EXISTS services(
     id SERIAL PRIMARY KEY,
     business_id INTEGER REFERENCES businesses(id),
     category_id INTEGER REFERENCES categories(id),
@@ -41,8 +43,41 @@ CREATE TABLE services(
     description TEXT,
     price NUMERIC(10,2),
     created_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
-    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL
+    updated_at TIMESTAMPTZ DEFAULT NOW() NOT NULL,
+    search_vector TSVECTOR
 );
 
+CREATE FUNCTION update_search_vector()
+RETURNS TRIGGER AS $$
+BEGIN
+      NEW.search_vector = to_tsvector('english', coalesce(NEW.name, '') || ' ' || coalesce(NEW.description, ''));
+      RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
 
+
+CREATE FUNCTION update_updated_at()
+RETURNS TRIGGER AS $$
+BEGIN
+     NEW.updated_at = NOW();
+     RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+
+CREATE TRIGGER update_users_updated_at
+BEFORE UPDATE ON users
+FOR EACH ROW EXECUTE FUNCTION update_updated_at();
+
+CREATE TRIGGER update_businesses_updated_at
+BEFORE UPDATE ON businesses
+FOR EACH ROW EXECUTE FUNCTION update_updated_at();
+
+CREATE TRIGGER update_services_updated_at
+BEFORE UPDATE ON services
+FOR EACH ROW EXECUTE FUNCTION update_updated_at();
+
+CREATE TRIGGER trigger_update_search_vector
+BEFORE UPDATE OR INSERT ON services
+FOR EACH ROW EXECUTE FUNCTION update_search_vector();
 
